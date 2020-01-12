5a66e872bb2e4841c43b9a1b90e14579d0bc2882
Jeff Collins <jcollins@coupledtech.com>
2020-01-12 15:06:52 -0800

    11

--- Services/Template/getDownloadUrl.js
+++ Services/Template/getDownloadUrl.js
@@ -21,6 +21,7 @@ module.exports = function getDownloadUrl(context, token, driveId, fileName) {
 
                     const result = JSON.parse(response.body);
                     context.log("RESULT ", result);
+                    context.log("RESULT VALUE ", result.value[0]);
                     if (result.value && result.value[0]) {
                         //context.log("RESULT VALUE ",result.value);
                         //context.log("RESULT VALUE[0] ",result.value[0]);
